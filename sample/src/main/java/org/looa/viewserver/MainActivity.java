76883c34bd010aaa758ca5f97e71239cc10ad9d1
looa <ran_v@foxmail.com>
2017-07-17 17:03:31 +0800

    init

---
+++ sample/src/main/java/org/looa/viewserver/MainActivity.java
@@ -0,0 +1,28 @@
+package org.looa.viewserver;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.debug.hv.ViewServer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewServer.get(this).addWindow(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ViewServer.get(this).setFocusedWindow(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+}
